06fbdecef1e8d3ed4d3053bff6066f663a0b6327
andrew sunada <kaleb2azriel@gmail.com>
2017-10-20 00:43:10 -0700

    Updating script to get all dancers from wsdc

--- src/server/scripts/processDancers.js
+++ src/server/scripts/processDancers.js
@@ -5,30 +5,53 @@ let dancerDef = require('../definitions/Dancer');
 let wsdc = wsdcConfig();
 let fireDB = fDB();
 let processDancerValues = (results) => {
-    let fetchDancers = () => {
-        let max = 5, i = 0, promises = [];        
-        while(i <= max){
-            let ci = results[i++];
-            while(!ci.hasOwnProperty('value')){
-                ci = results[i++];
-                max++;
+    let fetchDancers = (dancerSet) => {
+        let nextPromise = new Promise((resolve, reject) => {
+            let i = 0, promises = [];        
+            while(i < dancerSet.length){
+                let ci = dancerSet[i++];
+                while(!ci.hasOwnProperty('value')){
+                    ci = dancerSet[i++];                    
+                }
+                promises.push(wsdc.GetDancer(ci.value));
             }
-            console.log("KV: ", ci);
-            promises.push(wsdc.GetDancer(ci.value));
-        }
-        return promises;
+    
+            Promise.all(promises)
+                .then(results => {
+                    results.forEach((result) => {
+                        if(!result.hasOwnProperty('dancer') || !result.dancer.hasOwnProperty('wscid')) return;
+                        let dancer = new dancerDef(result);
+                        console.log("Dancer: ", dancer);
+                        fireDB.WriteDancerToFirebase(dancer.WSCID, dancer);
+                    });
+                    resolve("done");
+                })
+                .catch((error) => {
+                    reject("Problem with promise set: ", error);
+                });
+        });        
+        return nextPromise;
     };
-    let promises = fetchDancers();
-    Promise.all(promises)
-        .then(results => {
-            results.forEach((result) => {
-                console.log("Result: ", result);
-                if(!result.hasOwnProperty('dancer') || !result.dancer.hasOwnProperty('wscid')) return;
-                let dancer = new dancerDef(result);
-                console.log("Writing dancer: ", dancer);
-                fireDB.WriteDancerToFirebase(dancer.WSCID, dancer);
+
+    let run = (i, step) => {
+        if(i >= results.length){
+            console.log("Exit 1");
+            return;
+        }else if((i + step) >= results.length){
+            console.log("Exit 2");
+            run(i, results.length - 1);
+            return;
+        }
+        console.log(`Fetching: ${i} - ${i + step}`);
+        fetchDancers(results.slice(i, i + step))
+            .then(() => {
+                setTimeout(() => {
+                    run(i + step, step);
+                }, 2000);                
             });
-        });
+        
+    };
+    run(0, 10);
 };
 
 wsdc.GetDancers()
