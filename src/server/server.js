b794065b2a97aff06ea197031825ad33dc06a6f4
andrew sunada <kaleb2azriel@gmail.com>
2018-07-09 23:27:21 -0700

    Setting port to port 8080, removing unused logic

--- src/server/server.js
+++ src/server/server.js
@@ -54,6 +54,6 @@ app.use('/api/dancer', memcache(3600), DancerController);
 app.use('/api/account', AccountController);
 app.use('/api/events', EventsController);
 
-app.listen(9000, function() {
+app.listen(8080, function() {
 	console.log('listening to this joint on port 9000');
 });
