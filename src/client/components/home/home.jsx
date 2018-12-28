f738b7044d9ec12d685cb42a3bc45f4c65209ce0
andrew sunada <kaleb2azriel@gmail.com>
2018-12-27 21:29:29 -0800

    Adding images, copy-webpack-plugin

--- src/client/components/home/home.jsx
+++ src/client/components/home/home.jsx
@@ -8,151 +8,169 @@ import className from 'classnames';
 require('./home.scss');
 
 const DIVISIONS = [
-  { Key: 'champion', Label: 'Champions' },
-  { Key: 'allstar', Label: 'All-Stars' },
-  { Key: 'advanced', Label: 'Advanced' },
-  { Key: 'intermediate', Label: 'Intermediate' },
-  { Key: 'novice', Label: 'Novice' },
-  { Key: 'newcomer', Label: 'Newcomer' },
+	{ Key: 'champion', Label: 'Champions' },
+	{ Key: 'allstar', Label: 'All-Stars' },
+	{ Key: 'advanced', Label: 'Advanced' },
+	{ Key: 'intermediate', Label: 'Intermediate' },
+	{ Key: 'novice', Label: 'Novice' },
+	{ Key: 'newcomer', Label: 'Newcomer' },
 ];
 const ROLES = [
-  { Key: 'leader', Label: 'Leader' },
-  { Key: 'follower', Label: 'Follower' },
+	{ Key: 'leader', Label: 'Leader' },
+	{ Key: 'follower', Label: 'Follower' },
 ];
 
 const ApiService = new API();
 export class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      form: {
-        division: '',
-        role: '',
-        qualifies: false,
-      },
-      dancers: [],
-    };
-  }
-  searchDancers() {
-    let { division, role, qualifies } = this.state.form;
-    ApiService.GetDancers(division, role, qualifies)
-      .then((results) => {
-        this.setState({ dancers: results });
-      })
-      .catch((error) => {
-        console.log('Api error: ', error);
-      });
-  }
-  isSubmitAvailable() {
-    return this.state.form.division !== '' && this.state.form.role !== '';
-  }
-  onDivisionChange(division) {
-    let form = this.state.form;
-    console.log('Changing: ', division);
-    this.setState({ form: Object.assign({}, form, { division }) });
-  }
-  onRoleChange(role) {
-    let form = this.state.form;
-    this.setState({ form: Object.assign({}, form, { role }) });
-  }
-  onQualifiesChange($event) {
-    let form = this.state.form;
-    console.log('Qualfies: ', $event.target.checked);
-    this.setState({
-      form: Object.assign({}, form, { qualifies: $event.target.checked }),
-    });
-  }
-  onSubmitClicked() {
-    if (!this.isSubmitAvailable()) return;
-    this.searchDancers();
-  }
-  componentDidMount() {
-    FacebookApi.FetchUser().then((result) => {
-      console.log('User in component: ', result);
-    });
-  }
-  render() {
-    const getButtonClass = (isActive) =>
-      className('form-selector__action', 'btn', 'btn-light', {
-        'btn-success': isActive,
-      });
-    const getSubmitClass = (isSubmitable) =>
-      className('form-submit', 'btn', 'btn-light', {
-        'btn-primary': isSubmitable,
-      });
-    return (
-      <main className="strictly-container">
-        <header className="container-header">
-          <h1>Find my Strictly</h1>
-        </header>
-        <section className="container-body">
-          <div className="form-container">
-            <label className="form-label">Division</label>
-            <div className="form-selector division">
-              {DIVISIONS.map((division, index) => {
-                return (
-                  <button
-                    className={getButtonClass(
-                      division.Key === this.state.form.division,
-                    )}
-                    onClick={(e) => this.onDivisionChange(division.Key)}
-                    key={index}
-                  >
-                    {division.Label}
-                  </button>
-                );
-              })}
-            </div>
-          </div>
-          <div className="form-container">
-            <label className="form-label">Role</label>
-            <div className="form-selector role">
-              {ROLES.map((role, index) => {
-                return (
-                  <button
-                    className={getButtonClass(
-                      role.Key === this.state.form.role,
-                    )}
-                    onClick={(e) => this.onRoleChange(role.Key)}
-                    key={index}
-                  >
-                    {role.Label}
-                  </button>
-                );
-              })}
-            </div>
-          </div>
-          <div className="form-container parallel">
-            <label className="form-label">Qualifies: </label>
-            <div className="form-selector qualifies">
-              <label className="switch">
-                <input
-                  name="qualifies"
-                  type="checkbox"
-                  id="qualifies"
-                  onChange={(e) => this.onQualifiesChange(e)}
-                />
-                <span className="slider round" />
-              </label>
-            </div>
-          </div>
-          <button
-            className={getSubmitClass(this.isSubmitAvailable())}
-            onClick={() => this.onSubmitClicked()}
-          >
-            Submit
-          </button>
-          <ul className="dancer-list">
-            {this.state.dancers.map((dancer, index) => {
-              return (
-                <li key={index}>
-                  {dancer.FirstName} {dancer.LastName}
-                </li>
-              );
-            })}
-          </ul>
-        </section>
-      </main>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			form: {
+				division: '',
+				role: '',
+				qualifies: false,
+			},
+			searchPending: false,
+			dancers: [],
+		};
+	}
+	searchDancers() {
+		let { division, role, qualifies } = this.state.form;
+		this.setState({ searchPending: true });
+		ApiService.GetDancers(division, role, qualifies)
+			.then(results => {
+				this.setState({ dancers: results, searchPending: false });
+			})
+			.catch(error => {
+				this.setState({ searchPending: false });
+				console.log('Api error: ', error);
+			});
+	}
+	isSubmitAvailable() {
+		return this.state.form.division !== '' && this.state.form.role !== '';
+	}
+	onDivisionChange(division) {
+		let form = this.state.form;
+		this.setState({ form: Object.assign({}, form, { division }) });
+	}
+	onRoleChange(role) {
+		let form = this.state.form;
+		this.setState({ form: Object.assign({}, form, { role }) });
+	}
+	onQualifiesChange($event) {
+		let form = this.state.form;
+		this.setState({
+			form: Object.assign({}, form, { qualifies: $event.target.checked }),
+		});
+	}
+	onSubmitClicked() {
+		if (!this.isSubmitAvailable()) return;
+		this.searchDancers();
+	}
+	componentDidMount() {
+		FacebookApi.FetchUser().then(result => {
+			console.log('User in component: ', result);
+		});
+	}
+	render() {
+		const getButtonClass = isActive =>
+			className('form-selector__action', 'btn', 'btn-light', {
+				'btn-success': isActive,
+			});
+		const getSubmitClass = isSubmitable =>
+			className('form-submit', 'btn', 'btn-light', {
+				'btn-primary': isSubmitable,
+			});
+		return (
+			<main className="strictly-container">
+				<header className="container-header">
+					<h1>Find my Strictly</h1>
+				</header>
+				<section className="container-body">
+					<div className="form-container">
+						<label className="form-label">Division</label>
+						<div className="form-selector division">
+							{DIVISIONS.map((division, index) => {
+								return (
+									<button
+										className={getButtonClass(
+											division.Key ===
+												this.state.form.division
+										)}
+										onClick={e =>
+											this.onDivisionChange(division.Key)
+										}
+										key={index}
+									>
+										{division.Label}
+									</button>
+								);
+							})}
+						</div>
+					</div>
+					<div className="form-container">
+						<label className="form-label">Role</label>
+						<div className="form-selector role">
+							{ROLES.map((role, index) => {
+								return (
+									<button
+										className={getButtonClass(
+											role.Key === this.state.form.role
+										)}
+										onClick={e =>
+											this.onRoleChange(role.Key)
+										}
+										key={index}
+									>
+										{role.Label}
+									</button>
+								);
+							})}
+						</div>
+					</div>
+					<div className="form-container parallel">
+						<label className="form-label">Qualifies: </label>
+						<div className="form-selector qualifies">
+							<label className="switch">
+								<input
+									name="qualifies"
+									type="checkbox"
+									id="qualifies"
+									onChange={e => this.onQualifiesChange(e)}
+								/>
+								<span className="slider round" />
+							</label>
+						</div>
+					</div>
+					<button
+						className={getSubmitClass(this.isSubmitAvailable())}
+						onClick={() => this.onSubmitClicked()}
+					>
+						Submit
+					</button>
+					{this.state.searchPending ? (
+						<div className="loading-container">
+							<img
+								className="loading"
+								src="/images/fidget-spinner.gif"
+							/>
+						</div>
+					) : (
+						<ul className="dancer-list">
+							{this.state.dancers.map((dancer, index) => {
+								return (
+									<li
+										className="dancer-list-item"
+										key={index}
+									>
+										{dancer.FirstName} {dancer.LastName}
+									</li>
+								);
+							})}
+						</ul>
+					)}
+				</section>
+			</main>
+		);
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,73 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'public');
 module.exports = {
-    entry: {
-        index: "./src/client/index.jsx",
-    },
-    output: {
-        path: BUILD_DIR,
-        filename: "[name].bundle.js",
-        sourceMapFilename: '[name].map'
-    },
-    module: {
-        loaders: [
-            {
-                test : /\.jsx|js?/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.html$/,
-                loader: "file-loader?name=[name].[ext]",
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpe?g|png)$/,
-                loader: "file-loader?name=[path][name].[ext]"
-            },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                exclude: /node_modules/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                exclude: /node_modules/,
-                loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file-loader?name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            }
-        ]
-    }
+	entry: {
+		home: './src/client/components/home/home.jsx',
+		account: './src/client/components/account/account.jsx',
+		index: './src/client/index.jsx',
+	},
+	output: {
+		path: BUILD_DIR,
+		filename: '[name].bundle.js',
+		sourceMapFilename: '[name].map',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx|js?/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.html$/,
+				loader: 'file-loader?name=[name].[ext]',
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader',
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				loaders: [
+					'style-loader',
+					'css-loader',
+					'resolve-url-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(jpe?g|png)$/,
+				loader: 'file-loader?name=[path][name].[ext]',
+			},
+			{
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				exclude: /node_modules/,
+				loader:
+					'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]',
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				exclude: /node_modules/,
+				loader:
+					'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]',
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file-loader?name=fonts/[name].[ext]',
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+			},
+		],
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: 'src/client/shared/assets/images', to: 'images' },
+		]),
+	],
 };
